d2a7a4f2b20eff6c1a305950bbef118c4ba05540
vyshakhm <vyshakhm817@gmail.com>
2025-07-18 16:10:10 +0530

    Settings added csrf origin

--- transcodenexus/settings.py
+++ transcodenexus/settings.py
@@ -126,6 +126,8 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+DATA_UPLOAD_MAX_MEMORY_SIZE = 104857600
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
